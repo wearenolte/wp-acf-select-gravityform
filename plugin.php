4fb2c3d025d443f3f8e65306b35e6128dd3e3884
mitogh <mitogh@gmail.com>
2016-04-27 21:57:00 -0500

    Add plugin initial files

---
+++ plugin.php
@@ -0,0 +1,19 @@
+<?php
+use Lean\Acf\Types\Gravity;
+
+/*
+Plugin Name: Advanced Custom Fields: Gravity Forms extension
+Plugin URI: https://github.com/moxie-leean/wp-acf-select-gravityform
+Description: ACF Type that allow to have a custom gravity form type
+Version: 0.1.0
+Author: Moxie
+Author URI: http://getmoxied.net/
+*/
+
+
+$url = plugin_dir_url( __FILE__ );
+$path = plugin_dir_path( __FILE__ );
+
+require_once $path . 'vendor/autoload.php';
+
+new Gravity\Select( $url, $path );
