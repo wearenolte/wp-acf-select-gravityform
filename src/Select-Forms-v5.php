6f478abd702886af6c7c274e473dc584429f2f1e
mitogh <mitogh@gmail.com>
2016-04-27 21:57:13 -0500

    Register a new type for ACF

---
+++ src/Select-Forms-v5.php
@@ -0,0 +1,67 @@
+<?php namespace Lean\Acf\Types\Gravity;
+
+/**
+ * Setup configuration of the ACF fields form.
+ */
+class Acf_Field_Select_Form extends \acf_field {
+
+	/*
+	 *  This function will setup the field type data
+	 */
+	function __construct( $settings ) {
+		/*
+		 *  name (string) Single word, no spaces. Underscores allowed
+		 */
+		$this->name = 'SELECT_GRAVITY_FORMS';
+		$this->label = 'Gravity Forms';
+		$this->category = 'choice';
+		$this->defaults = [];
+		$this->l10n = [
+			'error'	=> 'Select a valid option',
+		];
+		$this->settings = $settings;
+
+		parent::__construct();
+	}
+
+	/*
+	 *  Create the HTML interface for your field
+	 *
+	 *  @param	$field (array) the $field being edited
+	 *  @return	n/a
+	 */
+	function render_field( $field ) {
+		Select_Field::render( $field );
+	}
+
+	/*
+	 *  This filter is applied to the $value before it is saved in the db we make
+	 *  sure we save an int value that is positive, 0 is going to be the default
+	 *  value for non form so any value like null will be converted to 0.
+	 *
+	 *  @param	$value (mixed) the value found in the database
+	 *  @param	$post_id (mixed) the $post_id from which the value was loaded
+	 *  @param	$field (array) the field array holding all the field options
+	 *  @return	$value
+	 */
+	function update_value( $value, $post_id, $field ) {
+		return absint( $value );
+	}
+
+	/*
+	 *  This filter is appied to the $value after it is loaded from the db and
+	 *  before it is returned to the template. Make sure to return always an int
+	 *  value since when it comes from the DB it comes as string like '1.
+	 *
+	 *  @param	$value (mixed) the value which was loaded from the database
+	 *  @param	$post_id (mixed) the $post_id from which the value was loaded
+	 *  @param	$field (array) the field array holding all the field options
+	 *
+	 *  @return	$value (mixed) the modified value
+	 */
+	function format_value( $value, $post_id, $field ) {
+		return absint( $value );
+	}
+}
+new Acf_Field_Select_Form( $this->settings );
+?>
---
+++ src/Select_Field.php
@@ -0,0 +1,58 @@
+<?php namespace Lean\Acf\Types\Gravity;
+
+/**
+ * Definition of the markup of the field.
+ */
+class Select_Field {
+
+	public static function render( $field ) {
+		if ( ! class_exists( '\GFAPI' ) ) {
+			self::error( 'Make sure you instellaed and activated Gravity Forms' );
+			return;
+		}
+		$forms = \GFAPI::get_forms();
+		if ( empty( $forms ) ) {
+			self::error( 'You don\'t have any available form.' );
+			return;
+		}
+		self::select( $forms, $field );
+	}
+
+	protected static function error( $message ) {
+	?>
+		<strong><?php echo esc_html( $message ); ?></strong>
+	<?php
+	}
+
+	protected static function select( $forms, $field ) {
+	?>
+		<select name="<?php echo esc_attr( $field['name'] ); ?>">
+	<?php
+		self::option();
+		$selection = isset( $field['value'] ) ? absint( $field['value'] ) : 0;
+		foreach ( $forms as $form ) {
+			self::option([
+				'value' => $form['id'],
+				'label' => $form['title'],
+				'selected' => $selection === $form['id'],
+			]);
+		}
+	?>
+		</select>
+	<?php
+	}
+
+	protected static function option( $args = [] ) {
+		$args = wp_parse_args( $args, [
+			'selected' => false,
+			'value' => 0,
+			'label' => 'None',
+		]);
+		$selected = $args['selected'] ? 'selected' : '';
+	?>
+		<option value="<?php echo esc_attr( $args['value'] ); ?>" <?php echo esc_attr( $selected ); ?>>
+			<?php echo esc_html( $args['label'] ); ?>
+		</option>
+	<?php
+	}
+}
