b9f51e037fe3f15c3302fe35b32e5a21bdf24944
mitogh <mitogh@gmail.com>
2016-06-02 14:15:24 -0500

    Fix CI issues  - Fix #5

--- plugin.php
+++ plugin.php
@@ -1,4 +1,5 @@
 <?php namespace Lean\Acf\Types\Gravity;
+
 /*
 Plugin Name: Advanced Custom Fields: Gravity Forms extension
 Plugin URI: https://github.com/moxie-lean/wp-acf-select-gravityform
@@ -7,17 +8,15 @@ Version: 0.1.0
 Author: Moxie
 Author URI: http://getmoxied.net/
 */
-
-
-$url = plugin_dir_url( __FILE__ );
-$path = plugin_dir_path( __FILE__ );
 $autoload = $path . 'vendor/autoload.php';
 
-if ( file_exists(  ) ) {
+if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
 
 if ( class_exists( __NAMESPACE__ . '\\Select' ) ) {
+	$url = plugin_dir_url( __FILE__ );
+	$path = plugin_dir_path( __FILE__ );
 	new Select( $url, $path );
 } else {
 	add_action( 'admin_notices', function(){
@@ -26,6 +25,6 @@ if ( class_exists( __NAMESPACE__ . '\\Select' ) ) {
 		<p>Make sure you are including the <code>vendor/autoload.php</code> file for the <strong>ACF Gravity Form Select component.</strong></p>
 	</div>
 	<?php
-		deactivate_plugins( plugin_basename( __FILE__ )  );
+		deactivate_plugins( plugin_basename( __FILE__ ) );
 	});
 }
